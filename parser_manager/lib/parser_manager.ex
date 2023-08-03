0489f8cf28ac484bf522ee6dafc822ca41590a4e
조원우 <greenaloud@gmail.com>
2023-08-03 17:53:35 +0900

    parser manager init

---
+++ parser_manager/lib/parser_manager.ex
@@ -0,0 +1,18 @@
+defmodule ParserManager do
+  @moduledoc """
+  Documentation for `ParserManager`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ParserManager.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ parser_manager/test/parser_manager_test.exs
@@ -0,0 +1,8 @@
+defmodule ParserManagerTest do
+  use ExUnit.Case
+  doctest ParserManager
+
+  test "greets the world" do
+    assert ParserManager.hello() == :world
+  end
+end
